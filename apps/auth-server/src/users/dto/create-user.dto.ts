15d4fd6de5f4ac82a61400da3fa18e0200bb5197
Raphael Londner <rlondner@users.noreply.github.com>
2026-07-04 20:40:36 -0400

    fix(auth-server): add @MaxLength(254) to CreateUserDto.email (bug-0169)

--- apps/auth-server/src/users/dto/create-user.dto.ts
+++ apps/auth-server/src/users/dto/create-user.dto.ts
@@ -1,9 +1,9 @@
-import { ArrayUnique, IsArray, IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+import { ArrayUnique, IsArray, IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString() @MinLength(1) firstName: string;
   @IsString() @MinLength(1) lastName: string;
-  @IsEmail() email: string;
+  @IsEmail() @MaxLength(254) email: string;
   @IsString() @IsNotEmpty() orgId: string;
   @IsString() @IsOptional() username?: string;
   @IsString() @IsOptional() phoneNumber?: string;
